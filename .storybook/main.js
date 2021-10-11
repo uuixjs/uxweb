d5fb17dd382d6402669cfe45b28cd0ebbbe18049
cmuhoza <ceddymuhoza@gmail.com>
2021-10-11 16:27:08 -0400

    Add preview-head

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-google-analytics",
     "@storybook/preset-create-react-app",
     "../src/stories/_core/addon-features/register.tsx",
     "../src/stories/_core/addon-render-speed/register.tsx"
